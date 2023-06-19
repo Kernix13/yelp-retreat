75629f111c8508d5964ad73f0cc4c7f47783fc10
Jim <jimkernicky@gmail.com>
2023-06-19 08:07:17 -0400

    Removed comments, added semicolons

--- public/js/scrollToTop.js
+++ public/js/scrollToTop.js
@@ -1,25 +1,22 @@
-const scrollToTopBtn = document.querySelector(".scrollToTopBtn")
-let rootElement = document.documentElement
-let TOGGLE_RATIO = 0.05
+const scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+let rootElement = document.documentElement;
+let TOGGLE_RATIO = 0.05;
 
 function handleScroll() {
-  // do something on scroll
-  let scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+  let scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
   if ((rootElement.scrollTop / scrollTotal) > TOGGLE_RATIO) {
-    //show button
-    scrollToTopBtn.classList.add("showBtn")
+    scrollToTopBtn.classList.add("showBtn");
   } else {
-    //hide button
-    scrollToTopBtn.classList.remove("showBtn")
+    scrollToTopBtn.classList.remove("showBtn");
   }
 }
 
 function scrollToTop() {
-  //scroll to top logic
   rootElement.scrollTo({
     top: 0,
     behavior: "smooth"
   })
 }
-scrollToTopBtn.addEventListener("click", scrollToTop)
-document.addEventListener("scroll", handleScroll)
+
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
